e9a6d1be139f501f1978636cb8c9b2e3d2bead56
Joshua <joshuatt.chau@mail.utoronto.ca>
2025-09-20 11:46:46 -0400

    Activity 1.2: Added dynamic route ( /user/ < name > ) for personalized greeting

--- hello.py
+++ hello.py
@@ -1,9 +1,13 @@
 from flask import Flask
 app = Flask(__name__)
 
-@app.route("/")
-def home():
-    return "Hello, World!"
+@app.route('/')
+def index():
+    return '<h1>Hello World!</h1>'
 
-if __name__ == "__main__":
+@app.route('/user/<name>')
+def user(name):
+    return '<h1>Hello, {}!</h1>'.format(name)
+
+if __name__ == '__main__':
     app.run(debug=True)
