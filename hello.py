d3b1d2125b5afe98b3c9188eb75029751cffa418
Joshua <joshuatt.chau@mail.utoronto.ca>
2025-09-20 14:29:21 -0400

    Activity 1.3: navbar + greeting + LLLL timestamp, base template, 404/500

--- hello.py
+++ hello.py
@@ -7,9 +7,19 @@ app = Flask(__name__)
 Bootstrap(app)   # initialize Flask-Bootstrap
 Moment(app)      # initialize Flask-Moment
 
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('500.html'), 500
+
+
 @app.route("/")
 def index():
-    # use your real name here
+    # Default: use my real name here
     name = "Joshua"
     return render_template("user.html", name=name, current_time=datetime.utcnow())
 
@@ -17,6 +27,7 @@ def index():
 @app.route("/user/<name>")
 def user(name):
     return render_template("user.html", name=name, current_time=datetime.utcnow())
+
 
 if __name__ == "__main__":
     app.run(debug=True)
